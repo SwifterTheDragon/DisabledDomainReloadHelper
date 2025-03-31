8c3768cbeaccb22df9e21ddc36a4a5da2502c7e0
Swifter Spaan <66500938+SwifterTheDragon@users.noreply.github.com>
2025-03-31 04:45:11 -0400

    Introduce CleanupDisabledDomainReloadPhases enum

---
+++ src/SwifterTheDragon.DisabledDomainReloadHelper.SourceGenerator/Core/CleanupDisabledDomainReloadPhases.cs
@@ -0,0 +1,24 @@
+// Copyright SwifterTheDragon, 2025. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+namespace SwifterTheDragon.DisabledDomainReloadHelper.SourceGenerator.Core
+{
+    /// <summary>
+    /// Specifies which phase disabled domain reload cleanup should occur.
+    /// </summary>
+    public enum CleanupDisabledDomainReloadPhases
+    {
+        /// <summary>
+        /// The default value. This should never be used intentionally.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Specifies that cleanup should be performed upon exiting play mode.
+        /// </summary>
+        OnExitPlayMode = 1,
+        /// <summary>
+        /// Specifies that cleanup should be performed upon entering play mode.
+        /// </summary>
+        OnEnterPlayMode = 2
+    }
+}
